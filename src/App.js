e0891a22b2fa594434ddcfd5a85a9afc5da2dee8
Premal Katigar <premalkatigar@Premals-MacBook-Pro.local>
2022-02-11 22:55:24 +0530

    removed unwanted package

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-
 import './App.css';
 import AutoCompleteList from './containers/AutoCompleteList';
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <AutoCompleteList />
-      </QueryClientProvider>
+      <AutoCompleteList />
     </div>
   );
 }
